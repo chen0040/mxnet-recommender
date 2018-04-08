49c419281bfe55dfed450f3744866b87aa56b00b
xschen <xs0040@gmail.com>
2018-04-08 21:17:43 +0800

    train and predict scripts for content-based filtering with temporal information

---
+++ demo/temporal_content_based_filtering.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from mxnet_recommender.library.content_based_filtering import TemporalContentBasedFiltering
+
+
+def main():
+    data_dir_path = './data/ml-latest-small'
+    output_dir_path = './models'
+
+    records = pd.read_csv(data_dir_path + '/ratings.csv')
+    print(records.describe())
+
+    ratings_train, ratings_test = train_test_split(records, test_size=0.2, random_state=0)
+
+    timestamp_train = ratings_train.as_matrix(columns=['timestamp'])
+    item_id_train = ratings_train.as_matrix(columns=['movieId'])
+    rating_train = ratings_train.as_matrix(columns=['rating'])
+
+    print(timestamp_train.head())
+
+    timestamp_test = ratings_test.as_matrix(columns=['timestamp'])
+    item_id_test = ratings_test.as_matrix(columns=['movieId'])
+    rating_test = ratings_test.as_matrix(columns=['rating'])
+
+    max_item_id = records['movieId'].max()
+
+    cf = TemporalContentBasedFiltering()
+    cf.max_item_id = max_item_id
+    history = cf.fit(timestamp_train=timestamp_train,
+                     item_id_train=item_id_train,
+                     rating_train=rating_train,
+                     model_dir_path=output_dir_path,
+                     epochs=20,
+                     checkpoint_interval=2,
+                     batch_size=256)
+
+    metrics = cf.evaluate_mae(timestamp_test=timestamp_test,
+                              item_id_test=item_id_test,
+                              rating_test=rating_test)
+
+
+if __name__ == '__main__':
+    main()
---
+++ demo/temporal_content_based_filtering_predict.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from mxnet_recommender.library.content_based_filtering import TemporalContentBasedFiltering
+
+
+def main():
+    data_dir_path = './data/ml-latest-small'
+    trained_model_dir_path = './models'
+
+    records = pd.read_csv(data_dir_path + '/ratings.csv')
+    print(records.describe())
+
+    timestamp_test = records.as_matrix(columns=['timestamp'])
+    item_id_test = records.as_matrix(columns=['movieId'])
+    rating_test = records.as_matrix(columns=['rating'])
+
+    max_item_id = records['movieId'].max()
+
+    print(timestamp_test.head())
+
+    config = dict()
+    config['max_item_id'] = max_item_id
+
+    cf = TemporalContentBasedFiltering()
+    cf.load_model(model_dir_path=trained_model_dir_path)
+
+    predicted_ratings = cf.predict(item_id_test, timestamp_test)
+    print(predicted_ratings)
+
+    for i in range(20):
+        date = timestamp_test[i]
+        item_id = item_id_test[i]
+        rating = rating_test[i]
+        predicted_rating = cf.predict_single(item_id, date)
+        print('predicted: ', predicted_rating, ' actual: ', rating)
+
+
+if __name__ == '__main__':
+    main()
